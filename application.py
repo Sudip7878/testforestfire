4ec6e438bd174410b0b2e4d0fd268ad8b1977579
Sudip <112951953+Sudip7878@users.noreply.github.com>
2025-04-11 12:50:55 +0545

    update

--- application.py
+++ application.py
@@ -11,6 +11,9 @@ app=application
 ridge_model=pickle.load(open('models/ridge.pkl','rb'))
 standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
 
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 @app.route("/predictdata",methods=['GET','POST'])
 
